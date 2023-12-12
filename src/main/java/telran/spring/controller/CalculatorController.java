b01be6b225a34dc2cdfc98e6ba892500e14156f2
mlipov <maxim.lipov@gmail.com>
2023-12-12 18:15:40 +0200

    231212 HW-52 all calculator main arithmetic operators

--- src/main/java/telran/spring/controller/CalculatorController.java
+++ src/main/java/telran/spring/controller/CalculatorController.java
@@ -9,4 +9,19 @@ public class CalculatorController {
 	double add(double op1, double op2) {
 		return op1 + op2;
 	}
+
+	@GetMapping("subtract")
+	double subtract(double op1, double op2) {
+		return op1 - op2;
+	}
+
+	@GetMapping("multiply")
+	double multiply(double op1, double op2) {
+		return op1 * op2;
+	}
+
+	@GetMapping("divide")
+	double divide(double op1, double op2) {
+		return op1 / op2;
+	}
 }
